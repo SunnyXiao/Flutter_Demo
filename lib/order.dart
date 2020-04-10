17f1636183eb58982d00d3b1ead2b0f18c85c9e9
luisa.xiao <luisa.xiao@bamboonetworks.com>
2020-04-10 11:33:05 +0800

    init project

---
+++ lib/order.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class OrderWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return OrderWidgetState();
+  }
+}
+
+class OrderWidgetState extends State<OrderWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('order page'),
+    );
+  }
+}
